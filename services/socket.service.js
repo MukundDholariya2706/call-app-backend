54106bf2a4a912d6749638357895625f68534e2b
mukundtridhya <mukunddtridhyatech@gmail.com>
2024-01-12 16:58:37 +0530

    add new diff event for accepts and cut call

--- services/socket.service.js
+++ services/socket.service.js
@@ -5,8 +5,7 @@ const socketSetup = (server) => {
   io = require("socket.io")(server, {
     cors: {
       origin: [
-        "https://172.16.0.210:3001",
-        "https://call-app-backend.vercel.app",
+        "http://172.16.0.210:4200",
         "http://localhost",
         "http://localhost:4200",
         "https://call-app-frontend.vercel.app",
@@ -20,12 +19,18 @@ const socketSetup = (server) => {
   io.on("connection", (socket) => {
     userCount++;
     console.log("user connected", userCount);
+
+    // diconnected event
     socket.on("disconnect", () => {
       userCount--;
       console.log("user disconnected", userCount);
     });
 
     socket.on("room", async (obj) => {
+      console.log(io.sockets.adapter.rooms, "total room");
+      var rooms = io.sockets.adapter.rooms;
+      var room = rooms.get(obj.id);
+
       socket.join(obj.id);
 
       socket.on("sendMessage", async (messageObj) => {
@@ -41,6 +46,56 @@ const socketSetup = (server) => {
           console.log(error, "error");
         }
       });
+
+      socket.on("callInit", async (calledDetails) => {
+        const { fromUser, toUser } = calledDetails;
+        
+        socket.to(toUser?._id).emit("anyOneCalling", {
+          callerDetails: fromUser,
+          receiverDetails: toUser,
+        });
+      });
+
+      socket.on("callendFromCaller", async (calledDetails) => {
+        const { fromUser, toUser, callend } = calledDetails;
+
+        socket.to(toUser?._id).emit("callendFromCallerEmit", { callend });
+      });
+
+      socket.on("callendFromReceiver", async (calledDetails) => {
+        const { fromUser, toUser, callend } = calledDetails;
+
+        socket.to(toUser?._id).emit("callendFromReceiverEmit", { callend });
+      });
+
+      socket.on("receiverPickUpCall", async (calledDetails) => {
+        const { fromUser, toUser } = calledDetails;
+
+        socket
+          .to(toUser?._id)
+          .emit("receiverPickUpCallEmit", { callPickUp: true });
+      });
+    });
+
+    socket.on("ready", (roomName) => {
+      console.log("READY");
+      console.log(roomName, "roomName");
+      socket.to(roomName.reciverUser).emit("ready", roomName);
+    });
+
+    socket.on("candidate", ({ candidate, roomName }) => {
+      console.log("candidate");
+      socket.to(roomName).emit("candidate", candidate);
+    });
+
+    socket.on("offer", ({ offer, roomName }) => {
+      console.log("OFFER");
+      socket.to(roomName).emit("offer", offer);
+    });
+
+    socket.on("answer", ({ answer, roomName }) => {
+      console.log("ANSWER");
+      socket.to(roomName).emit("answer", answer);
     });
   });
 };
